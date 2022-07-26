274d7c0675786cd40d1b5054526c8569e31f6388
alasdairkite93 <101645489+alasdairkite93@users.noreply.github.com>
2022-07-26 19:20:27 +0100

    Solution is correct
    The 3n + 1 problem
    Consider the following algorithm to generate a sequence of numbers. Start with an integer n. If n is even, divide by 2. If n is odd, multiply by 3 and add 1. Repeat this process with the new value of n, terminating when n = 1. For example, the following sequence of numbers will be generated for n = 22:
    22 11 34 17 52 26 13 40 20 10 5 16 8 4 2 1
    It is conjectured (but not yet proven) that this algorithm will terminate at n = 1 for every integer n. Still, the conjecture holds for all integers up to at least 1, 000, 000.
    For an input n, the cycle-length of n is the number of numbers generated up to and including the 1. In the example above, the cycle length of 22 is 16. Given any two numbers i and j, you are to determine the maximum cycle length over all numbers between i and j, including both endpoints.
    Input
    The input will consist of a series of pairs of integers i and j, one pair of integers per line. All integers will be less than 1,000,000 and greater than 0.
    Output
    For each pair of input integers i and j, output i, j in the same order in which they appeared in the input and then the maximum cycle length for integers between and including i and j. These three numbers should be separated by one space, with all three numbers on one line and with one line of output for each line of input.

---
+++ Iterations and lists/Iterations and lists/3n+1Problem.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Iterations_and_lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                foreach (int i in Input(10, 1))
+                {
+                    //Console.WriteLine("{0}", i);
+                }
+        }
+        public static System.Collections.Generic.IEnumerable<int> Input(int stat, int finish)
+        {
+
+            Console.WriteLine("Input start:");
+            int start = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input end:");
+            int end = Convert.ToInt32(Console.ReadLine());
+
+            List<int> Output = new List<int>();
+
+            IEnumerable<int> results = Enumerable.Range(start, end).Select(x => x);
+
+            foreach (int r in results)
+            {
+                //Console.WriteLine("\nr value:{0}", r);
+                int result = r;
+
+                while (result != 1)
+                {
+                    result = result % 2 == 0 ? result / 2 : (result * 3) + 1;
+                    Output.Add(result);
+                    yield return result;
+                }
+            }
+
+            IEnumerable<int> distintOutput = Output.Distinct();
+
+
+            Console.Clear();
+            Console.WriteLine("{0}   {1}   {2}", start, end, distintOutput.Count());
+        }
+    }
+}
